4786688b594202d99deee6615d2b3fa28e66b911
JoJo149z <j.mueller.richmann@icloud.com>
2026-04-16 14:24:41 +0200

    fixed setStartDirection and added checkMoveCollision (with death animation)

--- LightCyclesBase.java
+++ LightCyclesBase.java
@@ -6,8 +6,8 @@ public class LightCyclesBase extends Actor {
     int speed;
     String farbe;
 
-    LightCyclesBase(int speed,  String farbe) {
-        setStartDirection("down");
+    LightCyclesBase(int speed,  String farbe, String setStartDirection) {
+        setStartDirection(setStartDirection);
         setImage(("Lightcycles"+farbe+".png"));
         this.farbe = farbe;
         this.speed = speed;
@@ -26,17 +26,21 @@ public class LightCyclesBase extends Actor {
         switch (direction) {
             case "left":
                 setRotation(180);
+                break;
             case "right":
                 setRotation(0);
+                break;
             case "up":
                 setRotation(270);
+                break;
             case "down":
                 setRotation(90);
+                break;
         }
     }
 
     public void handelMovement() {
-        checkCollision();
+        checkMoveCollision();
         String key = Greenfoot.getKey();
         if (key != null) {
             if ("a".equals(key)) {
@@ -50,8 +54,26 @@ public class LightCyclesBase extends Actor {
         getWorld().addObject(new ImageObject("LightcyclesTrail"+farbe+".png"), getX(), getY());
     }
 
-    public void checkCollision() {
-        if (isTouching(ImageObject.class)){
+    public void checkMoveCollision() {
+        int x = 0;
+        int y = 0;
+        int Rotation = getRotation();
+        switch (Rotation) {
+            case 90:
+                y = y + 15;
+                break;
+            case 180:
+                x = x - 15;
+                break;
+            case 270:
+                y = y - 15;
+                break;
+            case 0:
+                x = x + 15;
+                break;
+        }
+        if (getOneObjectAtOffset(x,y,ImageObject.class)!=null){
+            speed = 0;
             setImage(("LightcyclesExplosion1.png"));
             setImage(("LightcyclesExplosion2.png"));
         }
