d743cb41be7c3e925ff5d81a5be5a8202bf43d21
nengel24 <noel.engel@rlg.berlin>
2026-04-23 14:13:13 +0200

    Schüsse Programmiert

--- GridBugsSpinne.java
+++ GridBugsSpinne.java
@@ -12,8 +12,21 @@ public class GridBugsSpinne extends Actor
      * Act - do whatever the GridBugsSpinne wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    
+    int timer = 0;
+    
     public void act()
     {
-        // Add your action code here.
+        if(isTouching(GridBugsKugel.class)){
+            timer++;
+        }
+        
+        if(timer != 0){
+            timer++;
+        }
+        
+        if(timer == 6){
+            getWorld().removeObject(this);
+        }
     }
 }
