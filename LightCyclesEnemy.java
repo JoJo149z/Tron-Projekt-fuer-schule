5b97c1f6ff6c6b94ba5d40210ccf293e98991028
JoJo149z <j.mueller.richmann@icloud.com>
2026-04-23 13:35:01 +0200

    added subclasses that will be coded in next commit

---
+++ LightCyclesEnemy.java
@@ -0,0 +1,29 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class LightCyclesEnemy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LightCyclesEnemy extends LightCyclesBase
+{
+    /**
+     *
+     * @param speed
+     * @param farbe             Gelb/Blau
+     * @param setStartDirection left, up, down, right
+     */
+    LightCyclesEnemy(int speed, String farbe, String setStartDirection) {
+        super(speed, farbe, setStartDirection);
+    }
+
+    /**
+     * Act - do whatever the LightCyclesEnemy wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
---
+++ LightCyclesPlayer.java
@@ -0,0 +1,29 @@
+import greenfoot.*;
+
+/**
+ * Write a description of class LightCyclesPlayer here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class LightCyclesPlayer extends LightCyclesBase
+{
+    /**
+     *
+     * @param speed
+     * @param farbe             Gelb/Blau
+     * @param setStartDirection left, up, down, right
+     */
+    LightCyclesPlayer(int speed, String farbe, String setStartDirection) {
+        super(speed, farbe, setStartDirection);
+    }
+
+    /**
+     * Act - do whatever the LightCyclesPlayer wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
